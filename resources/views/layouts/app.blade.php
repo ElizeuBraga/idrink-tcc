2306ef328155d15f3a46dcded0086398a8341087
Elizeu <elizeubragasantos@gmail.com>
2019-06-14 09:34:59 -0300

    migrate nao funciona, logo, links navegacao

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,13 +18,18 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        .navbar-brand > img{
+            width: 60px;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="imgs/logo.svg" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -33,7 +38,15 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Entregas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">Relatórios</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
