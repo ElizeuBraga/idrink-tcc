b1d2b62c44ac1c91083e5489d8f61e300443d306
Elizeu <elizeubragasantos@gmail.com>
2019-06-17 13:16:09 -0300

    migration deliveries

---
+++ database/migrations/2019_06_17_154625_create_deliveries_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeliveriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliveries', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('status',['open','canceled','delivered']);
+
+            $table->unsignedBigInteger('adress_id');
+            $table->foreign('adress_id')->references('id')->on('adresses')->onDelete('cascade');
+            
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('deliveries', function (Blueprint $table) {
+            $table->dropForeign('user_id');
+        });
+
+        Schema::dropIfExists('deliveries');
+    }
+}
