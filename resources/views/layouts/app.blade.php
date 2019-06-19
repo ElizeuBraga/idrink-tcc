f8a4189f3f7b183ebddf89a689d8476db38d358a
Elizeu <elizeubragasantos@gmail.com>
2019-06-19 13:35:55 -0300

    adicionando  class"active" nos links navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,13 +55,13 @@
                     <!-- Left Side Of Navbar -->
                     @if(Auth::check())
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                        <a class="nav-link active" href="{{route('delivery')}}">Entregas</a>
+                        <li class="nav-item {{ Request::is('entregas') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{route('delivery')}}">Entregas</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('produtos') ? 'active' : '' }}">
                             <a class="nav-link" href="{{route('product')}}">Produtos</a>
                         </li>
-                        <li class="nav-item">
+                        <li class="nav-item {{ Request::is('relatorios') ? 'active' : '' }}">
                             <a class="nav-link" href="{{route('report')}}">Relatórios</a>
                         </li>
                     </ul>
@@ -81,7 +81,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{-- {{ Auth::user()->name }} <span class="caret"></span> --}}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
