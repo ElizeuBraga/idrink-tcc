d54f59d710399ea069dd5cca6e69b027f69d1103
Elizeu <elizeubragasantos@gmail.com>
2019-06-21 08:59:30 -0300

    mudancas para erro no register

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::group(['prefix' => 'users'], function(){
 Route::group(['middleware' => 'auth:api'], function(){
     Route::get('index', ['uses' => 'UserController@index']);
 });
+
 Route::get('deliveries', ['uses' => 'DeliveryController@index'])->middleware('auth:api');
 Route::post('login', ['uses' => 'UserController@login']);
 Route::get('logout', ['uses' => 'UserController@logout'])->middleware('auth:api');
