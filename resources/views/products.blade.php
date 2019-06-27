495b47d4df96e1e09cae084a00843aee91393e40
Elizeu <elizeubragasantos@gmail.com>
2019-06-27 10:35:03 -0300

    Botoes para as tabelas de produtos

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,6 +2,9 @@
 @section('content')
 <div class="container">
     <h1>Produtos</h1>
+    <all-products></all-products>
+    <a href="#" class="btn btn-primary" style="margin: 0px 10px 0px 0px">Novo</a>
+    <a href="#" class="btn btn-primary" style="margin: 0px 10px 0px 0px">Ativos</a>
+    <a href="#" class="btn btn-primary" style="margin: 0px 10px 0px 0px">Inaivos</a>
 </div>
-<all-products></all-products>
 @endsection
