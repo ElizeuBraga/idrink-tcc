5eb9b1a8cea57bc20c7be6a27a3c1a14b8c4fc4e
Elizeu <elizeubragasantos@gmail.com>
2019-06-27 16:55:27 -0300

    redirectTo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/entregas';
 
     /**
      * Create a new controller instance.
