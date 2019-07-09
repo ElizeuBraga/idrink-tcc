9b7dc49b6c756c29a3f28921108fb9cb921cd987
Elizeu <elizeubragasantos@gmail.com>
2019-07-09 02:52:17 -0300

    Adicionei um novo arquivo de css para tela de login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/mystyle.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/loginscreen.css') }}" rel="stylesheet">
     <style>
         .navbar-brand>img {
             width: 60px;
@@ -60,54 +61,53 @@
             <a class="navbar-brand" href="{{ url('/') }}">
                 <img src="/imgs/logo.svg" alt="">
             </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse"
-            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            @if(Auth::check())
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item {{ Request::is('entregas') ? 'active' : '' }}">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                @if(Auth::check())
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item {{ Request::is('entregas') ? 'active' : '' }}">
                         <a class="nav-link" href="{{route('delivery')}}">Entregas</a>
                     </li>
-                    <li class="nav-item {{ Request::is(['produtos','produtos/ativos','produtos/inativos']) ? 'active' : '' }}">
+                    <li
+                        class="nav-item {{ Request::is(['produtos','produtos/ativos','produtos/inativos']) ? 'active' : '' }}">
                         <a class="nav-link" href="{{route('allProducts')}}">Produtos</a>
                     </li>
                     <li class="nav-item {{ Request::is('relatorios') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{route('report')}}">Relatórios</a>
-                        </li>
-                    </ul>
+                        <a class="nav-link" href="{{route('report')}}">Relatórios</a>
+                    </li>
+                </ul>
+                @endif
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
                     @endif
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
-                        @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                    @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->storename }} <span class="caret"></span>
                         </a>
-                        
+
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                style="display: none;">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
                         </div>
@@ -115,12 +115,12 @@
                     @endguest
                 </ul>
             </div>
-        </div>
+    </div>
     </nav>
-        <main class="py-4 container" style="margin-top:70px; margin-bottom:60px;">
-            @yield('content')
-        </main>
-@yield('scrip')
+    <main class="py-4 container" style="margin-top:70px; margin-bottom:60px;">
+        @yield('content')
+    </main>
+    @yield('scrip')
 </body>
 
 </html>
