65d6cc9104ae46ca99a2acf93d4d7e612496500f
Elizeu <elizeubragasantos@gmail.com>
2019-07-31 13:57:50 -0300

    animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
     @extends('layouts.app')
     @section('content')
                 <img src="/imgs/welcome/beer2.jpg" alt="Beer" style="width: 100%; height: 600px">
-                <div class="centered">iDrink</div>
+                <div class="centered"><div class="w3-animate-zoom">iDrink</div></div>
 
         <p></p>
     @endsection
