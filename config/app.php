72614a8014196f24bca6644add5f269e5099f670
Elizeu <elizeubragasantos@gmail.com>
2019-08-29 16:08:48 -0300

    login usuario

--- config/app.php
+++ config/app.php
@@ -176,7 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Barryvdh\Cors\ServiceProvider::class,
-
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
