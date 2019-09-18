a616f4b7fa76929e5a65227093f68a0c30ee94e3
Elizeu <elizeubragasantos@gmail.com>
2019-09-18 16:00:00 -0300

    routes api e readme

--- readme.md
+++ readme.md
@@ -8,17 +8,13 @@ imagens gratis
 https://unsplash.com/photos/UErWoQEoMrc
 
 # Rotas Api
-* POST .../new  New user
-
-* POST .../users/login  Login
-* GET .../users/logout  Logout
-
-* GET .../users/{user_id} get user for edit
-* PATCH .../{user_id}/update edit a user
-* GET .../all/stores return all store
-* GET .../getstore/{store_name} return a store by name
-* GET .../products/{user_id} return products by store_id
-* GET .../deliveries/all return deliveries to the usr logged
-
-* GET .../all Return all deliveries of a logged user
-* GET .../store/{store_id}return deliveries of a logged user group by store_id
+* POST .../users/new                             New user
+* POST .../users/login/                          Login
+* GET .../users/logout/                          Logout
+* GET .../users/{user_id}/                       get user for edit
+* PATCH .../users/{user_id}/update/              edit a user
+* GET .../users/all/stores/                      return all store
+* GET .../users/getstore/{store_name}/           return a store by name
+* GET .../users/products/{user_id}/              return products by store_id
+* GET .../users/deliveries/all/                  return deliveries to the usr logged
+* GET ...users/deliveries/store/{store_id}/      return deliveries of a logged user group by store_id
--- routes/api.php
+++ routes/api.php
@@ -22,20 +22,18 @@ Route::group(['prefix' => 'users'], function(){
     Route::post('/login', ['uses' => 'UserController@login']); // Login
 
     Route::group(['middleware' => 'auth:api'], function(){
-        Route::get('logout', ['uses' => 'UserController@logout']); // Logout
-        Route::get('{user}', ['uses' => 'UserController@edit']); // get user for edit
-        Route::patch('{user}/update', ['uses' => 'UserController@update']); //edit a user
+        Route::get('/logout', ['uses' => 'UserController@logout']); // Logout
+        Route::get('/{user}', ['uses' => 'UserController@edit']); // get user for edit
+        Route::patch('/{user}/update', ['uses' => 'UserController@update']); //edit a user
 
         Route::get('/all/stores', ['uses' => 'UserController@allStores']); //return all store
-        Route::get('getstore/{name}', ['uses' => 'UserController@getStoreName']); //return a store by name
-        Route::get('products/{user_id}', ['uses' => 'UserController@storeProducts']); //return products by store_id
-        Route::get('deliveries/all', ['uses' => 'UserController@usrReportDeliveries']); // return deliveries to the usr logged
+        Route::get('/getstore/{name}', ['uses' => 'UserController@getStoreName']); //return a store by name
+        Route::get('/products/{user_id}', ['uses' => 'UserController@storeProducts']); //return products by store_id
+
+        Route::group(['prefix' => 'deliveries'], function(){
+            Route::get('/all', ['uses' => 'DeliveryController@getDeliveriesCustomer']); //Return all deliveries of a logged user
+            Route::get('/store/{id}', ['uses' => 'DeliveryController@getDeliveriesCustomerStoreId']); //return deliveries of a logged user group by store_id
+        });
     });
 });
 
-Route::group(['prefix' => 'deliveries'], function(){
-    Route::group(['middleware' => 'auth:api'], function(){
-    Route::get('all', ['uses' => 'DeliveryController@getDeliveriesCustomer']); //Return all deliveries of a logged user
-    Route::get('/store/{id}', ['uses' => 'DeliveryController@getDeliveriesCustomerStoreId']); //return deliveries of a logged user group by store_id
-    });
-});
