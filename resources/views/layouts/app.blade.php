70d0e55c45e6e4e48b7c810c7cfc3d7959488c8e
Elizeu <elizeubragasantos@gmail.com>
2019-09-20 15:50:33 -0300

    style products

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,12 +8,13 @@
 	<head>
 		<title>iDrink</title>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
         <!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
         <style>
+
             body{
                 background:linear-gradient(120deg, #f6d365 0%, #fda085 100%)
             }
@@ -25,6 +26,17 @@
             header{
                 background:linear-gradient(120deg, #f6d365 0%, #fda085 100%)
                 height: 12%!important;
+            }
+
+            button{
+                background-color: inherit;
+                color: inherit !important;
+                align-content: center;
+            }
+
+            button:hover{
+                background-color: inherit;
+                color: inherit !important;
             }
 
             #nav{
@@ -50,30 +62,30 @@
 	<body class="landing">
         <!-- Header -->
         <header id="header" class="alt" style="">
-            <h1><a href="/" style="color:black">iDrink</a></h1>
-            <a href="#nav" style="color:black">Menu</a>
-        </header>
-                <!-- Nav -->
-        <nav id="nav" style="">
-            <ul class="links">
-            @auth
-                <li><a href="/home">Home</a></li>
-            <li><a href="{{route('allProducts')}}">Produtos</a></li>
-                <li><a href="/relatorios">Relatórios</a></li>
-                <li>
+                <h1><a href="/" style="color:black">iDrink</a></h1>
+                <a href="#nav" style="color:black">Menu</a>
+            </header>
+            <!-- Nav -->
+            <nav id="nav" style="">
+                    <ul class="links">
+                        @auth
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="{{route('allProducts')}}">Produtos</a></li>
+                        <li><a href="/relatorios">Relatórios</a></li>
+                        <li>
                     <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        {{ __('Sair') }}
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    {{ __('Sair') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
                 </li>
-            @endauth
-            @guest
-            <li><a href="/login">Login</a></li>
-            <li><a href="/register">Cadastre-se</a></li>
-            @endguest
+                @endauth
+                @guest
+                <li><a href="/login">Login</a></li>
+                <li><a href="/register">Cadastre-se</a></li>
+                @endguest
             </ul>
         </nav>
         <div class="container">
@@ -83,11 +95,13 @@
 		<!-- One -->
 
 		<!-- Scripts -->
-			<script src="js/jquery.min.js"></script>
-			<script src="js/skel.min.js"></script>
-			<script src="js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="js/main.js"></script>
+        <script src="https://kit.fontawesome.com/e9e7f80931.js"></script>
+        <script src="js/jquery.min.js"></script>
+        <script src="js/skel.min.js"></script>
+        <script src="js/util.js"></script>
+        <!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+        <script src="js/main.js"></script>
+        @yield('script')
 
 	</body>
-</html>
+    </html>
