b494d69d5a4abd4eddd98ce2e9a724564193df9b
Elizeu <elizeubragasantos@gmail.com>
2019-10-04 15:17:00 -0300

    all users for testes

--- app/Http/Controllers/Web/UserController.php
+++ app/Http/Controllers/Web/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Web;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UserController extends Controller
 {
@@ -13,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::where('type', 'store')->get();
+        return dd($users);
     }
 
     /**
