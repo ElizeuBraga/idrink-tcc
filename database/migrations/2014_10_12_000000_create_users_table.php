9cab46ecde8456eabb4a6dfc6b63205552272c83
Elizeu <elizeubragasantos@gmail.com>
2019-10-09 13:59:34 -0300

    pwd_temp

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('cnpj')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('temp_pwd')->nullable();
             $table->string('api_token', 60)->unique();
             $table->rememberToken();
             $table->timestamps();
