aae7142063b969a60216c7282dbc6b41b514b4a7
Elizeu <elizeubragasantos@gmail.com>
2019-10-12 15:12:48 -0300

    address seed

--- database/factories/AdressesFactory.php
+++ database/factories/AdressesFactory.php
@@ -8,8 +8,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Address::class, function (Faker $faker) {
     return [
-        'address' => $faker->address,
-        'cep' => rand(1000000, 2000000),
+        'cep' => rand(10000000, 20000000),
+        'uf' => 'DF',
+        'logradouro' => $faker->name,
+        'bairro' => $faker->name,
+        'localidade' => $faker->name,
+        'complemento' => $faker->name,
+        'numero' => rand(1, 50),
         'latitude' => rand(1000, 2000),
         'longitude' => rand(1000, 2000),
         'user_id' => rand(1, 10)
